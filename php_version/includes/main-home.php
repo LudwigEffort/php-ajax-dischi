65bb40e6c4d450fa94b1acd658902bf70f55630a
LudwigEffort <dr.feynman65@gmail.com>
2022-12-18 22:14:25 +0100

    add foreach for discs (firs test)

--- php_version/includes/main-home.php
+++ php_version/includes/main-home.php
@@ -1,4 +1,14 @@
 <main><?php
     include_once __DIR__ . '/../db/data.php';
-?>
+
+    //echo $arrDiscs[0]['title'];
+
+    foreach ($arrDiscs as $disc) { ?>
+        <div>
+            <h2><?= $disc['title'] ?></h2>
+            <img src="<?= $disc['poster'] ?>" alt="">
+            <p><?= $disc['author'] ?></p>
+        </div> <?
+    } ?>
+
 </main>
--- php_version/index.php
+++ php_version/index.php
@@ -6,13 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Discs</title>
     <link rel="stylesheet" href="../assets/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 <body><?php
     include __DIR__ . '/includes/header.php';
 
-    //main
+    include_once __DIR__ . '/includes/main-home.php';
 
-    include __DIR__ . '/includes/footer.php' ?>
+    include __DIR__ . '/includes/footer.php' ?>;
 
 </body>
 </html>

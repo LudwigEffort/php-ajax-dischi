b86e7358bb13bafe2e58eb783526290a4b68f9db
LudwigEffort <dr.feynman65@gmail.com>
2022-12-18 22:58:07 +0100

    add navbar and main style

--- php_version/includes/header.php
+++ php_version/includes/header.php
@@ -1,3 +1,20 @@
 <header>
-    I'm header
+    <nav>
+        <div class="container text-center p-2">
+        <div class="row justify-content-between">
+            <div class="col-1">
+                <a class="navbar-brand" href="#">
+                    <img src="../assets/spotify-logo.png" alt="Bootstrap" width="40" height="40">
+                </a>
+            </div>
+            <div class="col-3">
+            <select class="form-select" aria-label="Default select example">
+                <option value="none">
+                    Chose genre
+                </option>
+            </select>
+            </div>
+        </div>
+        </div>
+    </nav>
 </header>
--- php_version/includes/main-home.php
+++ php_version/includes/main-home.php
@@ -1,14 +1,15 @@
-<main><?php
+<main class="container text-center p-4"><?php
     include_once __DIR__ . '/../db/data.php';
 
     //echo $arrDiscs[0]['title'];
 
     foreach ($arrDiscs as $disc) { ?>
-        <div>
-            <h2><?= $disc['title'] ?></h2>
-            <img src="<?= $disc['poster'] ?>" alt="">
-            <p><?= $disc['author'] ?></p>
+        <div class="row row-cols-4 g-5">
+            <div>
+                <h2><?= $disc['title'] ?></h2>
+                <img src="<?= $disc['poster'] ?>" alt="">
+                <p><?= $disc['author'] ?></p>
+            </div> 
         </div> <?
     } ?>
-
 </main>
